c0a64ceaf5bd88ead5df8868eee2b4a4e5c5b352
linguofeng <linguofeng@msn.com>
2012-08-24 15:20:01 +0800

    create android-quickstart-scala module

---
+++ android-quickstart-scala/src/main/resources/archetype-resources/src/main/scala/com/linguofeng/android/scala/MainActivity.scala
@@ -0,0 +1,18 @@
+package com.linguofeng.android.scala
+
+import android.os.Bundle
+import android.app.Activity
+import android.view.Menu
+
+class MainActivity extends Activity {
+
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+  override def onCreateOptionsMenu(menu: Menu) = {
+    getMenuInflater().inflate(R.menu.activity_main, menu)
+    true
+  }
+}
